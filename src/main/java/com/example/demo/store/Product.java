52cbba5361e0b1bc4e2401d9b1879953f1777616
springdev2024 <springdev2024@gmail.com>
2025-11-11 16:02:27 +0545

    Product edit feature

--- src/main/java/com/example/demo/store/Product.java
+++ src/main/java/com/example/demo/store/Product.java
@@ -1,6 +1,8 @@
 package com.example.demo.store;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 /**
@@ -10,15 +12,17 @@ import jakarta.persistence.Id;
 public class Product {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	private String name;
 	private Double price;
 
-	public Product(String name, Double price) {
-		this.name = name;
-		this.price = price;
+	public int getId() {
+		return id;
 	}
-	
-	public Product() {
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -36,4 +40,13 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product() {
+	}
+
 }
--- src/main/java/com/example/demo/store/ProductController.java
+++ src/main/java/com/example/demo/store/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,22 +17,33 @@ public class ProductController {
 	@GetMapping("/products")
 	public String getProducts(Model model) {
 		model.addAttribute("products", repository.findAll());
+		model.addAttribute("forUpdate", false);
+		model.addAttribute("product", new Product());
 		return "products.html";
 	}
 	
 	@PostMapping("/product")
-	public String saveNewProduct(@RequestParam("name") String name, @RequestParam("price") Double price) {
+	public String saveNewProduct(Product product) {
 		/**
 		 * code to store the data into database
 		 */
-		System.out.println("Name = " + name);
-		System.out.println("Price = " + price);
+//		System.out.println("Name = " + produ);
+//		System.out.println("Price = " + price);
 				
-		Product p = new Product(name, price);
-		
-		repository.save(p);
-		
+		repository.save(product);
 		return "redirect:/products";
 	}
-
+	
+	
+	/**
+	 * Mapping to display edit form
+	 */
+	@GetMapping("/products/edit/{name}")
+	public String getProductEditPage(@PathVariable("name") String productName, Model model) {
+		model.addAttribute("products", repository.findAll());
+		model.addAttribute("forUpdate", true);
+		model.addAttribute("product", repository.findByName(productName));
+		return "products.html";
+	}
+	
 }
