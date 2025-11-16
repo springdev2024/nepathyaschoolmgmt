b9781b83c9277b5b5e3a9da41bc471e039699a25
springdev2024 <springdev2024@gmail.com>
2025-11-16 12:38:03 +0545

    Search product feature

--- src/main/java/com/example/demo/store/ProductController.java
+++ src/main/java/com/example/demo/store/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.demo.store;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ public class ProductController {
 
 	@GetMapping("/products")
 	public String getProducts(Model model) {
+		// SELECT * FROM products
 		model.addAttribute("products", repository.findAll());
 		model.addAttribute("forUpdate", false);
 		model.addAttribute("product", new Product());
@@ -25,7 +28,21 @@ public class ProductController {
 	@PostMapping("/product")
 	public String saveNewProduct(Product product) {
 		// upsert object in database
-		repository.save(product);
+		
+		// TODO: check if productRepository already contains a product with given name
+//		if(repository.existsByName(product.getName())) {
+//			model.addAttribute("error", "Product with given name already exists");
+//		}
+		
+		//TODO: should check for validation errors
+//		if(valid) {
+//			// save & redirect
+//		} else {
+//			// send error message on the page
+//		}
+//		
+		repository.save(product);  // Actually saved in database
+		// INSERT INTO product (name, price) VALUES ('Apple M2', 240000);
 		return "redirect:/products";
 	}
 	
@@ -35,6 +52,10 @@ public class ProductController {
 	 */
 	@GetMapping("/products/edit/{id}")
 	public String getProductEditPage(@PathVariable("id") int productID, Model model) {
+		// productID = 7
+		// SELECT * FROM product WHERE id = 7
+		// This query returns a row and Hibernate encapsulates that row into Product object
+		// Product(7, "Apple M2", 240000)
 		model.addAttribute("product", repository.findById(productID).get());
 		model.addAttribute("products", repository.findAll());
 		model.addAttribute("forUpdate", true);
@@ -45,6 +66,16 @@ public class ProductController {
 	public String deleteProductByID(@RequestParam("id") int productID) {
 		repository.deleteById(productID);
 		return "redirect:/products";
+	}
+	
+	@GetMapping("/products/search")
+	public String getProductSearchResultPage(@RequestParam("query") String searchQuery,
+			Model model) {
+		List<Product> products = repository.findByNameContainingIgnoreCase(searchQuery);
+		model.addAttribute("products", products);
+		model.addAttribute("forUpdate", false);
+		model.addAttribute("product", new Product());
+		return "products.html";
 	}
 	
 }
--- src/main/java/com/example/demo/store/ProductRepository.java
+++ src/main/java/com/example/demo/store/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.store;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
 	Product findByName(String productName);
+
+	boolean existsByName(String name);
+
+	List<Product> findByNameContainingIgnoreCase(String searchQuery);
 
 }
